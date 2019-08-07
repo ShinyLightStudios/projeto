421ffcffb432f3b9e99c0a4e7aad39dc18356fe8
ShinyLightStudios <51110491+ShinyLightStudios@users.noreply.github.com>
2019-08-07 00:57:27 -0300

    07/08/2019
    Alterações feitas em reunião.

---
+++ projeto 07082019/procQuery.php
@@ -0,0 +1,52 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "chatprog";
+    $result_array = array();
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if(!$conn){
+        die("Erro na conexão" . mysqli_connect_error());
+    }
+
+    $sql = $_POST['sql'];
+    //echo "SQL: " . $sql . "<br>";
+    $comandoSql = explode(" ", $sql);
+    $comandosPermitidos = array("SELECT", "INSERT","UPDATE","DELETE");
+    if(in_array(strtoupper($comandoSql[1]),$comandosPermitidos)){
+        if (strtoupper($comandoSql[1]) == "SELECT"){
+            $result = mysqli_query($conn, $sql);
+            if ($result){
+                if(mysqli_num_rows($result)>0){
+                    while($row = mysqli_fetch_assoc($result)){
+                        array_push($result_array, $row);
+                    }
+                }
+            }else{
+                echo "Erro na consulta: ";
+                var_dump($result);
+            }
+            echo json_encode($result_array);
+        }
+        else{
+            $result = mysqli_query($conn, $sql);
+            if($result){
+                $result_array[0] = "TRUE";
+                echo json_encode($result_array);
+            }
+            else{
+                $result_array[0] = "FALSE";
+                echo json_encode($result_array);
+            }
+        }
+    }
+    else{
+        $result_array[0] = "FALSE";
+        $result_array[1] = strtoupper($comandoSql[1])." não é um comando permitido.";
+        echo json_encode($result_array);
+    }
+    mysqli_close($conn);
+
+?>
